ab76dc3334a4c73e28db1010862152c0bc5fa3cb
simon dabin <simon.dabin@pole-emploi.fr>
2021-10-21 15:51:38 +0200

    ajout eslint

--- src/square-of-squares/main.js
+++ src/square-of-squares/main.js
@@ -1,9 +1,9 @@
-let isSquare = function(n){
-    if (n > 0 || n == 0){
-      return Number.isInteger(Math.sqrt(n))
-    }else {
-      return false
-    }
+const isSquare = function(n) {
+  if (n > 0 || n == 0) {
+    return Number.isInteger(Math.sqrt(n));
+  } else {
+    return false;
   }
+};
 
-export default isSquare
+export default isSquare;
